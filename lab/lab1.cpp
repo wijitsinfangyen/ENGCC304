eecae5f8bf1a06d93b11ff419c8e528ec7e84e58
wijitsinfangyen <wijitsinfangyen@gmail.com>
2025-07-04 15:43:46 +0700

    lab

---
+++ lab/lab1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char Name[50];
+    int Age = 0;
+
+    printf("Enter your name: ");
+    scanf("%s", Name);  // รับชื่อ (แบบไม่มีช่องว่าง)
+
+    printf("Enter your age: ");
+    scanf("%d", &Age);
+
+    printf("- - - - - -\n");
+    printf("Hello %s\n", Name);
+    printf("Age = %d\n", Age);
+
+    return 0;
+}

9b0d17c0db187793774b1b290245138cd8a45298
wijitsinfangyen <wijitsinfangyen@gmail.com>
2025-10-12 21:39:08 +0700

    Add files via upload

---
+++ sturdy/leb1-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+        printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+        printf("Answer = %d\n", a + b + c);
+    return 0;
+}
